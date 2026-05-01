9b1010a43be64ce8b4007fb5b8d9a67cf93cdf46
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-05-01 19:29:14 +0600

    deployed Spinner components

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -5,10 +5,10 @@ const Loading = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="flex flex-col items-center gap-4">
         {/* Spinner */}
-        <div className="w-12 h-12 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
+        <div className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
 
         {/* Text */}
-        <p className="text-gray-600 text-6xl md:text-base">
+        <p className="text-gray-600 text-sm sm:text-base md:text-lg lg:text-xl">
           Loading, please wait...
         </p>
       </div>
